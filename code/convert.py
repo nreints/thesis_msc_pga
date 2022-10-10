fix quaternion convertion

code/convert.py
@@ -69,11 +69,11 @@ def quat2pos(quat, start_pos):
 
         rotated_start = fast_rotVecQuat(start_pos, quat[:,:4])
 
-        X_start, Y_start, Z_start = start_pos[0][:, 0], start_pos[0][:, 1], start_pos[0][:, 2]
-        X_rot, Y_rot, Z_rot = rotated_start[0][:, 0], rotated_start[0][:, 1], rotated_start[0][:, 2]
+        # X_start, Y_start, Z_start = start_pos[0][:, 0], start_pos[0][:, 1], start_pos[0][:, 2]
+        # X_rot, Y_rot, Z_rot = rotated_start[0][:, 0], rotated_start[0][:, 1], rotated_start[0][:, 2]
 
-        distance_start = ((X_start[0] - X_start[1])**2 + (Y_start[0] - Y_start[1])**2 + (Z_start[0] - Z_start[1])**2)**0.5
-        distance_rot = ((X_rot[0] - X_rot[1])**2 + (Y_rot[0] - Y_rot[1])**2 + (Z_rot[0] - Z_rot[1])**2)**0.5
+        # distance_start = ((X_start[0] - X_start[1])**2 + (Y_start[0] - Y_start[1])**2 + (Z_start[0] - Z_start[1])**2)**0.5
+        # distance_rot = ((X_rot[0] - X_rot[1])**2 + (Y_rot[0] - Y_rot[1])**2 + (Z_rot[0] - Z_rot[1])**2)**0.5
 
         # print(distance_start)
         # print(distance_rot)
@@ -93,8 +93,8 @@ def quat2pos(quat, start_pos):
         out = torch.empty((quat.shape[1], batch, vert_num, dim)).to(device)
 
         for frame in range(quat.shape[1]):
-            rotated_start = fast_rotVecQuat(start_pos, quat[:,frame,:4])
-            repeated_trans = torch.repeat_interleave(quat[:,frame,4:], repeats=8, dim=0)
+            rotated_start = fast_rotVecQuat(start_pos, quat[:, frame, :4]).reshape(-1, 3)
+            repeated_trans = torch.repeat_interleave(quat[:, frame, 4:], repeats=8, dim=0)
             out[frame] = (rotated_start + repeated_trans).reshape((batch, vert_num, dim))
 
         # Batch first
@@ -202,8 +202,6 @@ def diff_pos_start2pos(true_preds, start_pos):
     result = start_pos + true_preds
     return result.reshape(result.shape[0], 8, 3)
 
-
-
 def convert(true_preds, start_pos, data_type):
     """
     Converts true predictions given data type.
@@ -217,7 +215,8 @@ def convert(true_preds, start_pos, data_type):
 
     """
     if data_type == "pos" or data_type == "pos_norm":
-        return true_preds.reshape(true_preds.shape[0], 8,3)
+        return true_preds
+        # return true_preds.reshape(true_preds.shape[0], 8, 3)
     elif data_type == "eucl_motion":
         return eucl2pos(true_preds, start_pos)
     elif data_type == "quat":

code/lstm.py
@@ -15,17 +15,16 @@ class LSTM(nn.Module):
     def __init__(self, in_size, config):
         super().__init__()
         # Initialize the modules we need to build the network
-        self.n_layers = config.n_layers
-        self.hidden_size = config.hidden_size
+        self.n_layers = config["n_layers"]
+        self.hidden_size = config["hidden_size"]
         self.in_size = in_size
-        self.lstm = nn.LSTM(in_size, self.hidden_size, batch_first=True, dropout=config.dropout, num_layers=config.n_layers)
+        self.lstm = nn.LSTM(in_size, self.hidden_size, batch_first=True, dropout=config["dropout"], num_layers=config["n_layers"])
         self.layers = nn.Sequential(
             nn.Linear(self.hidden_size, in_size)
         )
 
     def forward(self, x, hidden_state=None):
         # Perform the calculation of the model to determine the prediction
-        # print(x.shape)
 
         batch_size, _, _ = x.shape
         if hidden_state == None:
@@ -68,8 +67,10 @@ class MyDataset(data.Dataset):
                 data_all = pickle.load(f)["data"]
                 data = data_all[self.data_type]
                 for frame in range(len(data) - (self.n_frames_perentry + 1)):
-
-                    self.start_pos.append(data_all["pos"][0])
+                    if self.data_type == "pos_diff_start":
+                        self.start_pos.append(data_all["pos"][0])
+                    else:
+                        self.start_pos.append(data_all["start"])
                     train_end = frame + self.n_frames_perentry
                     self.data.append(data[frame:train_end].reshape(-1, self.n_datap_perframe))
                     self.target.append(data[frame+1:train_end+1].reshape(-1, self.n_datap_perframe))
@@ -104,6 +105,7 @@ def train_model(model, optimizer, data_loader, test_loader, loss_module, num_epo
         loss_epoch = 0
         for data_inputs, data_labels, start_pos in data_loader:
             data_inputs = data_inputs.to(device)
+
             data_labels = data_labels.to(device)
             start_pos = start_pos.to(device)
 
@@ -205,7 +207,7 @@ def make(config, ndata_dict, loss_dict, optimizer_dict):
 
 if __name__ == "__main__":
 
-    n_sims = 750
+    n_sims = 1000
     sims = {i for i in range(n_sims)}
     train_sims = set(random.sample(sims, int(0.8 * n_sims)))
     test_sims = sims - train_sims
@@ -213,25 +215,26 @@ if __name__ == "__main__":
 
 
     config = dict(
-        learning_rate = 0.01,
-        epochs = 400,
+        learning_rate = 0.005,
+        epochs = 75,
         batch_size = 128,
         dropout = 0,
         loss_type = "L1",
         loss_reduction_type = "mean",
         optimizer = "Adam",
-        data_type = "pos",
+        data_type = "quat",
         architecture = "lstm",
         train_sims = list(train_sims),
         test_sims = list(test_sims),
-        n_frames = 20,
+        n_frames = 30,
         n_sims = n_sims,
         n_layers = 1,
         hidden_size = 96
         )
 
     loss_dict = {'L1': nn.L1Loss,
-                    'L2': nn.MSELoss}
+                    'L2': nn.MSELoss
+                }
 
     optimizer_dict = {'Adam': torch.optim.Adam}
 
@@ -245,6 +248,6 @@ if __name__ == "__main__":
 
     model = model_pipeline(config, ndata_dict, loss_dict, optimizer_dict)
     model_dict = {'config': config,
-                'data_dict':ndata_dict,
+                'data_dict': ndata_dict,
                 'model': model.state_dict()}
     torch.save(model_dict, f"models/{config['data_type']}_{config['architecture']}.pickle")

code/new_mujoco.py
@@ -186,6 +186,10 @@ def generate_data(string, n_steps, visualize):
         if i % 10 == 0:
             # Collect position data
             dataset["pos"][i//10] = get_vert_coords(sim, geom_id, xyz_local).T
+            # print(get_vert_coords(sim, geom_id, xyz_local).T[0,2] - prev[0, 2])
+            # if get_vert_coords(sim, geom_id, xyz_local).T[0, 2] > prev[0, 2]:
+            #     print("flying away")
+            #     exit()
 
             # Collect euclidean motion data
             dataset["eucl_motion"][i//10] = np.append(get_mat(sim, geom_id), sim.data.body_xpos[geom_id])
@@ -200,6 +204,7 @@ def generate_data(string, n_steps, visualize):
                 dataset["pos_diff"][i//10] = get_vert_coords(sim, geom_id, xyz_local).T - prev
 
                 prev = get_vert_coords(sim, geom_id, xyz_local).T
+
                 dataset["pos_diff_start"][i//10] = get_vert_coords(sim, geom_id, xyz_local).T - start
 
 
@@ -217,10 +222,10 @@ def generate_data(string, n_steps, visualize):
 def write_data_nsim(num_sims, n_steps, obj_type, visualize=False):
     for sim_id in range(num_sims):
         print("sim: ", sim_id)
-        euler = f"{np.random.uniform(-80, 80)} {np.random.uniform(-80, 80)} {np.random.uniform(-80, 80)}"
+        euler = f"{np.random.uniform(-40, 40)} {np.random.uniform(-40, 40)} {np.random.uniform(-40, 40)}"
         # euler = f"{0.0} {0.0} {0.0}"
-        pos = f"{np.random.uniform(-10, 10)} {np.random.uniform(-10, 10)} {np.random.uniform(4, 30)}"
-        size = f"{np.random.uniform(5, 10)} {np.random.uniform(5, 10)} {np.random.uniform(5, 10)}"
+        pos = f"{np.random.uniform(-10, 10)} {np.random.uniform(-10, 10)} {np.random.uniform(10, 30)}"
+        size = f"{np.random.uniform(0.5, 5)} {np.random.uniform(0.5, 5)} {np.random.uniform(0.5, 5)}"
         # print("size object", size)
 
         string = create_string(euler, pos, obj_type, size)

code/plot_data.py
@@ -2,7 +2,8 @@ from platform import architecture
 import torch
 import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d import Axes3D
-from torch_nn import Network
+from torch_nn import fcnn
+from lstm import LSTM
 import pickle
 from random import randint
 import numpy as np
@@ -15,11 +16,16 @@ from pyquaternion import Quaternion
 
 def load_model(data_type, architecture):
     # Load model
+
     model_dict = torch.load(f"models/{data_type}_{architecture}.pickle")
     config = model_dict['config']
     ndata_dict = model_dict['data_dict']
 
-    model = Network(ndata_dict[config['data_type']], config)
+    if architecture == "fcnn":
+        model = fcnn(ndata_dict[config['data_type']], config)
+    elif architecture == "lstm":
+        model = LSTM(ndata_dict[config['data_type']], config)
+
     model.load_state_dict(model_dict['model'])
     model.eval()
     print("Current model: \n", model)
@@ -60,10 +66,15 @@ def get_random_sim_data(data_type, nr_frames):
 
     return plot_data, original_data, plot_data2, start_pos[0]
 
-def get_prediction(original_data, data_type, xyz_data, start):
+def get_prediction_fcnn(original_data, data_type, xyz_data, start):
     # Collect prediction of model given simulation
     # Result should be xyz data for plot
+    # print("doiw")
+    # print(plot_data.shape)
+    # print(original_data.shape)
+    # print(xyz_data.shape)
     result = torch.zeros_like(xyz_data)
+    # print("iweoh")
 
     # Get first position
     start_pos = start[None, :]
@@ -72,15 +83,55 @@ def get_prediction(original_data, data_type, xyz_data, start):
         # Get 20 frames shape: (1, 480)
         input_data = original_data[frame_id - 20 : frame_id]
 
-        input_data = input_data.flatten()[None, :]
+        input_data = input_data.unsqueeze(dim=0)
+        #### fcnn
+        input_data = input_data.flatten(start_dim=1)
 
         # Save the prediction in result
         with torch.no_grad(): # Deactivate gradients for the following code
-            prediction = model(input_data)
+            prediction, hidden = model(input_data, hidden)
 
-            result[frame_id] = convert(prediction, start_pos, data_type)
+            result[frame_id] = convert(prediction, start_pos, data_type).reshape(-1, 8, 3)
 
     return result
+
+def get_prediction_lstm(original_data, data_type, xyz_data, start, nr_frames, out_is_in=False):
+    # Collect prediction of model given simulation
+    # Result should be xyz data for plot
+    frames, vert_num, dim = xyz_data.shape
+
+    # Because LSTM predicts 1 more frame
+    result = torch.zeros((frames+1, vert_num, dim))
+
+
+    # Get first position
+    start_pos = start[None, :]
+    hidden = torch.zeros(1, 1, 96)
+    cell = torch.zeros(1, 1, 96) #TODO
+    # print(hidden.shape)
+
+    for frame_id in range(0, xyz_data.shape[0], nr_frames):
+        # Get 20 frames shape: (1, 480)
+        print("---")
+        if not out_is_in or frame_id == 0:
+            input_data = original_data[frame_id : frame_id + nr_frames]
+            input_data = input_data.unsqueeze(dim=0)
+        print("input", input_data.shape)
+
+        # Save the prediction in result
+        with torch.no_grad(): # Deactivate gradients for the following code
+            prediction, (hidden, cell) = model(input_data, (hidden, cell))
+            if out_is_in:
+                input_data = prediction
+            print("pred", prediction.shape)
+            print("converted", convert(prediction, start_pos, data_type).reshape(-1, 8, 3).shape)
+            print("out shape", result[frame_id + 1 : frame_id + 21].shape)
+            out_shape = result[frame_id + 1 : frame_id + 21].shape
+            print(convert(prediction, start_pos, data_type).reshape(-1, 8, 3)[:out_shape[0], :, :].shape)
+            result[frame_id + 1 : frame_id + nr_frames + 1] = convert(prediction, start_pos, data_type).reshape(-1, 8, 3)[:out_shape[0], :, :]
+
+    return result
+
 
 def plot_3D_animation(data, result, plot_data2):
     data = data
@@ -89,10 +140,7 @@ def plot_3D_animation(data, result, plot_data2):
     # Open figure
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    # Set the axis limits
-    ax.set_xlim3d(-15, 15)
-    ax.set_ylim(-15, 15)
-    ax.set_zlim(0, 50)
+
     ax.set_xlabel('$X$')
     ax.set_ylabel('$Y$')
     ax.set_xlabel('$Z$')
@@ -128,15 +176,24 @@ def plot_3D_animation(data, result, plot_data2):
     ax.plot(X_pred, Y_pred, Z_pred, c="r")
     ax.plot(X_check, Y_check, Z_check, c="black")
 
+
+    # xx, yy = np.meshgrid(range(-15, 15), range(-15, 15))
+    # zz = xx*0
+
+    # ax.plot_surface(xx, yy, zz)
+
+
+    ax.set_xlim3d(-15, 15)
+    ax.set_ylim(-15, 15)
+    ax.set_zlim(0, 50)
+
     def update(idx):
         ax.set_xlabel('$X$')
         ax.set_ylabel('$Y$')
         ax.set_xlabel('$Z$')
 
-        ax.set_xlim3d(-15, 15)
-        ax.set_ylim3d(-15, 15)
-        ax.set_zlim3d(0, 50)
-
+        if idx == 20:
+            print("poep")
 
         # Remove the previous scatter plot
         if idx != 0:
@@ -172,6 +229,15 @@ def plot_3D_animation(data, result, plot_data2):
         ax.plot(predicted_cube[:, 0], predicted_cube[:, 1], predicted_cube[:, 2], c="r")
         ax.plot(check_cube[:, 0], check_cube[:, 1], check_cube[:, 2], c="black")
 
+        # xx, yy = np.meshgrid(range(-15, 15), range(-15, 15))
+        # zz = xx*0
+
+        # ax.plot_surface(xx, yy, zz)
+
+        ax.set_xlim3d(-15, 15)
+        ax.set_ylim3d(-15, 15)
+        ax.set_zlim3d(0, 40)
+
 
     # Interval : Delay between frames in milliseconds.
     ani = animation.FuncAnimation(fig, update, 225, interval=100, repeat=False)
@@ -182,12 +248,15 @@ def plot_3D_animation(data, result, plot_data2):
 if __name__ == "__main__":
     nr_frames = 225
     data_type = "pos"
-    architecture = "fcnn"
+    architecture = "lstm"
 
     model = load_model(data_type, architecture)
 
     plot_data, ori_data, pos_data, start = get_random_sim_data(data_type, nr_frames)
 
-    prediction = get_prediction(ori_data, data_type, plot_data, start)
+    if architecture == "fcnn":
+        prediction = get_prediction_fcnn(ori_data, data_type, plot_data, start)
+    elif architecture == "lstm":
+        prediction = get_prediction_lstm(ori_data, data_type, plot_data, start, 5, out_is_in=True)
 
     plot_3D_animation(np.array(plot_data), np.array(prediction), np.array(pos_data))

code/torch_nn.py
@@ -6,16 +6,12 @@ import pickle
 import torch.utils.data as data
 import random
 from convert import *
-import wandb
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
+import wandb2
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
-class Network(nn.Module):
+class fcnn(nn.Module):
 
     def __init__(self, n_data, config):
         super().__init__()
@@ -165,7 +161,7 @@ def train_model(model, optimizer, data_loader, test_loader, loss_module, num_epo
             optimizer.step()
 
         # Log and print epoch every 10 epochs
-        if epoch % 1 == 0:
+        if epoch % 10 == 0:
             # Log to W&B
             train_log(loss_epoch/len(data_loader), epoch)
 
@@ -242,7 +238,7 @@ def make(config, ndata_dict, loss_dict, optimizer_dict):
     test_data_loader = data.DataLoader(data_set_test, batch_size=config.batch_size, shuffle=True, drop_last=False)
 
     # Make the model
-    model = Network(ndata_dict[config.data_type], config).to(device)
+    model = fcnn(ndata_dict[config.data_type], config).to(device)
 
     # Make the loss and optimizer
     criterion = loss_dict[config.loss_type](reduction=config.loss_reduction_type)
@@ -260,21 +256,21 @@ if __name__ == "__main__":
 
 
     config = dict(
-        learning_rate = 0.001,
+        learning_rate = 0.005,
         epochs = 50,
         batch_size = 128,
         loss_type = "L1",
         loss_reduction_type = "mean",
         optimizer = "Adam",
-        data_type = "quat",
+        data_type = "log_quat",
         architecture = "fcnn",
         train_sims = list(train_sims),
         test_sims = list(test_sims),
         n_frames = 20,
         n_sims = n_sims,
-        hidden_sizes = [128, 256, 128],
-        activation_func = ["ReLU", "ReLU", "ReLU"],
-        dropout = [0.6, 0.4, 0.4],
+        hidden_sizes = [128, 256],
+        activation_func = ["ReLU", "ReLU"],
+        dropout = [0.6, 0.8],
         batch_norm = [True, True, True]
     )
 