trial quaternion fix???

code/convert.py
@@ -4,7 +4,17 @@ import roma
 # import time
 
 
-def eucl2pos(eucl_motion, start_pos):
+def eucl2pos(eucl_motion, start_pos, xpos_start):
+    if len(eucl_motion.shape) == 2:
+        rotations = eucl_motion[:, :9].reshape(-1, 3, 3)
+        start_origin = start_pos.reshape(-1, 8, 3).mT - xpos_start.reshape(-1, 1, 3).mT
+        mult = torch.bmm(rotations, start_origin).mT
+        out = mult + eucl_motion[:, 9:].reshape(-1, 1, 3) + xpos_start.reshape(-1, 1, 3)
+        return out.flatten(start_dim=1)
+    print("ERRRRROOOOOOOOOORRRR")
+
+
+def eucl2pos_ori(eucl_motion, start_pos):
     """
     Transforms a batch of vectors by a rotation matrix and translation vector.
 
@@ -23,17 +33,11 @@ def eucl2pos(eucl_motion, start_pos):
     """
     # In case of fcnn
     if len(eucl_motion.shape) == 2:
-        # rotations = eucl_motion[:, :9].reshape(-1, 3, 3)
-        # mult = torch.bmm(rotations, start_pos.reshape(-1, 8, 3).mT).mT
+        rotations = eucl_motion[:, :9].reshape(-1, 3, 3)
+        mult = torch.bmm(rotations, start_pos.reshape(-1, 8, 3).mT).mT
 
-        # out = (mult + eucl_motion[:, 9:][:, None, :]).flatten(start_dim=1)
-        # return out
-
-        # rotations = eucl_motion[:, :9].reshape(-1, 3, 3)
-        # mult = torch.bmm(rotations, start_pos.reshape(-1, 8, 3).mT).mT
-        print(start_pos.reshape(-1, 8, 3).shape)
-        print(eucl_motion[:, 9:].shape)
-        return (start_pos.reshape(-1, 8, 3) + eucl_motion[:, 9:][:, None, :]).flatten(start_dim=1)
+        out = (mult + eucl_motion[:, 9:][:, None, :]).flatten(start_dim=1)
+        return out
 
     # In case of LSTM/GRU
     else:
@@ -80,7 +84,28 @@ def fast_rotVecQuat(v, q):
     return rotated_v
 
 
-def quat2pos(quat, start_pos):
+def quat2pos(quat, start_pos, xpos_start):
+    if len(quat.shape) == 2:
+        start_pos_shape = start_pos.shape
+        start_origin = (start_pos.reshape(-1, 8, 3) - xpos_start).reshape(
+            start_pos_shape
+        )
+        rotated_start = fast_rotVecQuat(
+            start_origin,
+            quat[:, :4],
+        )
+        print(quat[:, 4:])
+        repeated_trans = quat[:, 4:][:, None, :]
+        print(repeated_trans)
+
+        if torch.any(quat[:, 4:] != 0):
+            print(quat[:, 4:][quat[:, 4:] != 0])
+            exit()
+        out = rotated_start + repeated_trans + xpos_start
+        return out.flatten(start_dim=1)
+
+
+def quat2pos_ori(quat, start_pos):
     """
     Input:
         - quat: Original predictions (quaternion motion)
@@ -308,7 +333,7 @@ def diff_pos_start2pos(true_preds, start_pos):
     return result.squeeze()
 
 
-def convert(true_preds, start_pos, data_type):
+def convert(true_preds, start_pos, data_type, xpos_start=None):
     """
     Converts true predictions given data type.
     Input:
@@ -321,11 +346,13 @@ def convert(true_preds, start_pos, data_type):
     if data_type == "pos" or data_type == "pos_norm":
         return true_preds
     elif data_type == "eucl_motion":
-        return eucl2pos(true_preds, start_pos)
-    elif data_type == "eucl_motion_old":
-        return eucl2pos(true_preds, start_pos)
+        return eucl2pos(true_preds, start_pos, xpos_start)
+    elif data_type == "eucl_motion_ori":
+        return eucl2pos_ori(true_preds, start_pos)
     elif data_type == "quat":
-        return quat2pos(true_preds, start_pos)
+        return quat2pos(true_preds, start_pos, xpos_start)
+    elif data_type == "quat_ori":
+        return quat2pos_ori(true_preds, start_pos)
     elif data_type == "log_quat":
         return log_quat2pos(true_preds, start_pos)
     elif data_type == "dual_quat":

code/create_data.py
@@ -202,6 +202,7 @@ def create_empty_dataset(n_steps, half_size, mass, body_inertia):
         "size_massCentroid": np.empty((n_steps, 2, 3)),
         "size_squared_massCentroid": np.empty((n_steps, 2, 3)),
         "start": np.empty((8, 3)),
+        "xpos_start": np.empty((1, 3)),
     }
 
 
@@ -314,6 +315,7 @@ def generate_data(
 
             if i == 0:
                 start_xpos = copy.deepcopy(xpos)
+                dataset["xpos_start"] = start_xpos
 
                 start_xyz = global_pos
                 # print(start_xpos, start_xyz)
@@ -344,28 +346,25 @@ def generate_data(
                 # print(np.linalg.inv(start_rotMat) @ start_xpos)
                 # print(current_rotMat @ np.linalg.inv(start_rotMat) @ start_xpos)
                 rel_trans = xpos - start_xpos
-                print("rel trans\n", rel_trans)
+                # print("rel trans\n", rel_trans)
                 rel_rot = current_rotMat @ np.linalg.inv(start_rotMat)
-                print(
-                    "Same\n",
-                    current_rotMat @ np.linalg.inv(start_rotMat),
-                    "\n",
-                    np.linalg.inv(start_rotMat) @ current_rotMat,
-                )
                 # print(dataset["eucl_motion"][i][:, :9].shape)
                 dataset["eucl_motion"][i][:, :9] = rel_rot.flatten()
                 dataset["eucl_motion"][i][:, 9:] = rel_trans
-                print(f"rel rot\n {rel_rot}")
-                print(dataset["pos"][0].shape, start_xpos.shape)
-                print("rotated local\n", rel_rot @ xyz_local)
-                current_pos_recalculated = (
-                    (rel_rot @ (dataset["pos"][0].T - start_xpos[:, None])).T
-                    + rel_trans[None, :]
-                    + start_xpos[None, :]
-                )
-                print("current pos\n", current_pos_recalculated)
-                print("global pos\n", global_pos)
-                print(current_pos_recalculated - global_pos)
+                # print(f"rel rot\n {rel_rot}")
+                # #
+                # print(dataset["pos"][0])
+                # print(start_xyz)
+                # exit()
+                # TODO Opslaan start_xpos
+                # current_pos_recalculated = (
+                #     (rel_rot @ (dataset["pos"][0].T - start_xpos[:, None])).T
+                #     + start_xpos[None, :]
+                #     + rel_trans[None, :]
+                # )
+                # print("current pos\n", current_pos_recalculated)
+                # print("global pos\n", global_pos)
+                # print(current_pos_recalculated - global_pos)
                 rel_quaternion_pyquat = (
                     Quaternion(get_quat(data, body_id)) * Quaternion(start_quat).inverse
                 )
@@ -379,19 +378,35 @@ def generate_data(
                 dataset["rotation_axis_trans"][i][:3] = rotation_axis
                 dataset["rotation_axis_trans"][i][3:] = xpos
                 # dataset["rotation_axis_trans"][i] = np.append(rotation_axis, xpos)
-                quaternion = rel_quaternion_pyquat.elements
+                rel_quaternion = rel_quaternion_pyquat.elements
 
-                dataset["quat"][i][:, :4] = quaternion
+                ################################################################
+                # q_norm = rel_quaternion_pyquat.normalised
+                # rotated_v = np.zeros_like(dataset["pos"][0])
+                # for i in range(len(dataset["pos"][0])):
+                #     v = dataset["pos"][0][i] - start_xpos
+                #     rotated_v[i] = q_norm.rotate(v)
+                # recalculated_pos = rotated_v + start_xpos + rel_trans
+
+                # print("recalculated\n", recalculated_pos)
+                # print("global pos\n", global_pos)
+                # if any(rel_trans != 0):
+                #     print(rel_trans)
+                #     exit()
+
+                ################################################################
+
+                dataset["quat"][i][:, :4] = rel_quaternion
                 dataset["quat"][i][:, 4:] = rel_trans
                 # dataset["quat"][i] = np.append(quaternion, rel_trans)
 
                 # Collect Log Quaternion data
-                dataset["log_quat"][i][:, :4] = calculate_log_quat(quaternion)
+                dataset["log_quat"][i][:, :4] = calculate_log_quat(rel_quaternion)
                 dataset["log_quat"][i][:, 4:] = rel_trans
                 # dataset["log_quat"][i] = np.append(
                 #     calculate_log_quat(quaternion), rel_trans
                 # )
-                dualQuaternion = get_dualQ(quaternion, rel_trans)
+                dualQuaternion = get_dualQ(rel_quaternion, rel_trans)
                 # Collect Dual-Quaternion data
                 dataset["dual_quat"][i] = dualQuaternion
                 # Collect log_dualQ data (= bivector = rotation axis)
@@ -458,7 +473,7 @@ def get_sizes(symmetry):
     )  # TODO willen we dat ze gemiddeld even groot zijn? Ik heb nu dat de kortste zijde gemiddeld even groot is.
     sizes = ratio * random_size
     # print("sizes of cube:", "12 24 72")
-    return "3 3 3", [3, 3, 3]
+    # return "3 3 3", [3, 3, 3]
     return f"{sizes[0]} {sizes[1]} {sizes[2]}", sizes
 
 
@@ -597,13 +612,13 @@ def write_data_nsim(
             print(f"Generating sim {sim_id}/{num_sims-1}")
         # Define euler angle
         euler = f"{np.random.uniform(0, 360)} {np.random.uniform(0, 360)} {np.random.uniform(0, 360)}"
-        euler = "0 0 0"
+        # euler = "0 0 0"
         # print(f"initial orientation: {euler}")
         # Define sizes
         sizes_str, sizes_list = get_sizes(symmetry)
         # Define position
         pos = f"{np.random.uniform(-10, 10)} {np.random.uniform(-10, 10)} {np.random.uniform(-10, 10)}"
-        # pos = "0 0 0"
+        pos = "3 4 5"
         # print(f"initial position: {pos}")
         string = get_string(euler, pos, sizes_str, gravity, plane, integrator)
         # Create dataset
@@ -638,9 +653,9 @@ if __name__ == "__main__":
         help="symmetry of the box.\nfull: symmetric box 1:1:1\n; semi: 2 sides of same length, other longer 1:1:10\n;tennis: tennis_racket effect 1:3:10\n;none: random lengths for each side",
         default="tennis",
     )
-    parser.add_argument("-l_min", type=int, help="linear qvel min", default=10)
-    parser.add_argument("-l_max", type=int, help="linear qvel max", default=20)
-    parser.add_argument("-a_min", type=int, help="angular qvel min", default=4)
+    parser.add_argument("-l_min", type=int, help="linear qvel min", default=0)
+    parser.add_argument("-l_max", type=int, help="linear qvel max", default=0)
+    parser.add_argument("-a_min", type=int, help="angular qvel min", default=5)
     parser.add_argument("-a_max", type=int, help="angular qvel max", default=10)
     parser.add_argument(
         "-integrator",

code/fcnn.py
@@ -97,9 +97,13 @@ class MyDataset(data.Dataset):
                     self.target = torch.zeros((len_data, self.n_datap_perframe))
                     self.target_pos = torch.zeros((len_data, 24))
                     self.start_pos = torch.zeros_like(self.target_pos)
+                    self.xpos_start = torch.zeros((len_data, 3))
                 for frame in range(data_per_sim):
                     # Always save the start position for converting
-                    self.start_pos[count] = pos_data[0].flatten()
+                    self.start_pos[count] = torch.FloatTensor(pos_data[0].flatten())
+                    self.xpos_start[count] = torch.FloatTensor(
+                        data_all["xpos_start"].flatten()
+                    )
                     train_end = frame + self.n_frames_perentry
                     if self.extra_input[1] != 0:
                         extra_input_values = torch.FloatTensor(
@@ -123,6 +127,7 @@ class MyDataset(data.Dataset):
         self.normalize_extra_input = torch.mean(
             torch.norm(self.data[:, -self.extra_input[1] :], dim=1)
         )
+        # print(self.xpos_start.shape)
         # print("mean of norm extra_input", self.normalize_extra_input.item())
         # self.data[:, -self.extra_input[1] :] = self.extra_input_data
         print(f"The dataloader took {time.time() - start_time} seconds.")
@@ -137,8 +142,8 @@ class MyDataset(data.Dataset):
         data_target = self.target[idx]
         data_start = self.start_pos[idx]
         data_pos_target = self.target_pos[idx]
-        data_normalized = self.normalized_data[idx]
-        return data_point, data_target, data_start, data_pos_target, data_normalized
+        start_xpos = self.xpos_start[idx]
+        return data_point, data_target, data_start, data_pos_target, start_xpos
 
 
 def train_log(loss, epoch, config):
@@ -168,16 +173,22 @@ def train_model(
     for epoch in range(num_epochs):
         epoch_time = time.time()
         loss_epoch = 0
-        for data_inputs, data_labels, start_pos, pos_target, data_norm in data_loader:
+        for (
+            data_inputs,
+            data_labels,
+            start_pos,
+            pos_target,
+            xpos_start,
+        ) in data_loader:
 
             # Set data to current device
             data_inputs = data_inputs.to(
                 device
             )  # Shape: [batch, frames x n_data + config["extra_input_n"]]
-            data_norm = data_norm.to(device)
             data_labels = data_labels.to(device)  # Shape: [batch, n_data]
             pos_target = pos_target.to(device)  # Shape: [batch, n_data]
             start_pos = start_pos.to(device)  # Shape: [batch, n_data]
+            xpos_start = xpos_start.to(device)
             if config["str_extra_input"] == "inertia_body":
                 data_inputs[:, -config["extra_input_n"] :] = (
                     data_inputs[:, -config["extra_input_n"] :]
@@ -191,7 +202,17 @@ def train_model(
             # preds = preds * data_set_train.std + data_set_train.mean
 
             # Convert predictions to xyz-data
-            alt_preds = convert(preds, start_pos, data_loader.dataset.data_type)
+            if config.data_type[-3:] != "ori":
+                alt_preds = convert(
+                    preds,
+                    start_pos,
+                    data_loader.dataset.data_type,
+                    xpos_start,
+                )
+            # print(alt_preds)
+
+            else:
+                print("FIX THIS, NOT IMPLEMENTED YET!")
             # print("alt_preds:", alt_preds[0])
             # print("pos_targ", pos_target[0])
             # exit()
@@ -206,14 +227,14 @@ def train_model(
                 norm_penalty = 0
 
             position_loss = loss_module(alt_preds, pos_target)
-
             # Calculate the total loss
             loss = position_loss + norm_penalty
 
-            loss_epoch += loss
+            loss_epoch += position_loss
 
             # Perform backpropagation
             optimizer.zero_grad()
+            # loss = torch.tensor(loss, requires_grad=True)
             loss.backward()
 
             optimizer.step()
@@ -248,27 +269,40 @@ def eval_model(
                 total_loss = 0
                 total_convert_loss = 0
                 wandb_total_convert_loss = 0
-                for data_inputs, data_labels, start_pos, pos_target, _ in data_loader:
+                for (
+                    data_inputs,
+                    data_labels,
+                    start_pos,
+                    pos_target,
+                    xpos_start,
+                ) in data_loader:
                     if config["str_extra_input"] == "inertia_body":
                         data_inputs[:, -config["extra_input_n"] :] = (
                             data_inputs[:, -config["extra_input_n"] :]
                             / data_set_train.normalize_extra_input
                         )
                     # Set data to current device
-                    # data_inputs = data_inputs.to(device)
+                    data_inputs = data_inputs.to(device)
                     # data_norm = (data_inputs - data_set_train.mean) / data_set_train.std
                     data_labels = data_labels.to(device)
-
+                    xpos_start = xpos_start.to(device)
                     # Get predictions
                     preds = model(data_inputs)
                     # preds = model(data_norm)
                     preds = preds.squeeze(dim=1)
                     # preds = preds * data_set_train.std + data_set_train.mean
+                    print(data_labels[0][-3:])
 
                     # Convert predictions to xyz-data
-                    alt_preds = convert(
-                        preds.detach().cpu(), start_pos, data_loader.dataset.data_type
-                    )
+                    if config.data_type[-3:] != "ori":
+                        alt_preds = convert(
+                            preds.detach().cpu(),
+                            start_pos,
+                            data_loader.dataset.data_type,
+                            xpos_start,
+                        )
+                    else:
+                        print("FIX THIS, NOT IMPLEMENTED YET!")
 
                     # Determine norm penalty for quaternion data
                     if (
@@ -458,10 +492,10 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    print(args.data_dir_train)
+    # print(args.data_dir_train)
     data_dir_train = "data/" + " ".join(args.data_dir_train)
     # data_dir_train = "data/" + args.data_dir_train
-    print(data_dir_train)
+    print(f"Training on dataset: {data_dir_train}")
     # data_dirs_test = args.data_dir_test]
 
     # data_dirs_test = os.listdir("data")
@@ -470,7 +504,7 @@ if __name__ == "__main__":
 
     data_dirs_test = [
         " ".join(args.data_dir_train),
-        "data_tennis_pNone_gNone_tennisEffect",
+        # "data_tennis_pNone_gNone_tennisEffect",
     ]
     print(f"Testing on datasets: {data_dirs_test}")
 
@@ -510,7 +544,7 @@ if __name__ == "__main__":
         print(f"----- ITERATION {i+1}/{args.iterations} ------")
         # Divide the train en test dataset
         n_sims_train = len(os.listdir(data_dir_train))
-        n_sims_train = 4000
+        n_sims_train = 2000
         sims_train = {i for i in range(n_sims_train)}
         train_sims = set(random.sample(sims_train, int(0.8 * n_sims_train)))
         test_sims = sims_train - train_sims

code/plot_data.py
@@ -67,7 +67,7 @@ def get_random_sim_data(data_type, nr_sims, data_dir, i=None):
         i = randint(0, nr_sims - 1)
         print(f"Using random simulation number {i}, data_type {data_type}")
     else:
-        print(f"Using simulation {i}")
+        print(f"Using simulation {i}, data_type {data_type}")
 
     with open(f"{data_dir}/sim_{i}.pickle", "rb") as f:
         file = pickle.load(f)
@@ -83,7 +83,11 @@ def get_random_sim_data(data_type, nr_sims, data_dir, i=None):
             start_pos = torch.tensor(
                 file["data"]["pos"][0], dtype=torch.float32
             ).flatten()
+            start_xpos = torch.tensor(
+                file["data"]["xpos_start"], dtype=torch.float32
+            ).flatten()
             start_pos = start_pos[None, :].repeat(nr_frames, 1, 1)
+            start_xpos = start_xpos[None, :].repeat(nr_frames, 1, 1)
 
         if "rotation_axis_trans" in file["data"].keys():
             rot_axis_trans = file["data"]["rotation_axis_trans"]
@@ -92,15 +96,29 @@ def get_random_sim_data(data_type, nr_sims, data_dir, i=None):
             rot_axis_trans = None
 
         # Load the data in correct data type
-        original_data = torch.tensor(
-            file["data"][data_type], dtype=torch.float32
-        ).flatten(start_dim=1)
-        if data_type[-3:] == "ori":
-            data_type = data_type[:-4]
+        original_data = torch.FloatTensor(file["data"][data_type]).flatten(start_dim=1)
+        if data_type == "quat":
+            print("shape original_data: ", original_data.shape)
+            data_in_file = file["data"][data_type].squeeze()
+            print("shape data_in_file: ", data_in_file.shape)
+            print(np.any(data_in_file[:, 4:] != 0))
+            print(torch.any(original_data[:][4:] != 0))
+            exit()
+        # if data_type[-3:] == "ori":
+        #     data_type = data_type[:-4]
         # Convert to xyz position data for plotting
-        plot_data = convert(original_data, start_pos, data_type).reshape(
-            nr_frames, 8, 3
-        )
+        if (
+            data_type[-3:] != "ori"
+            and data_type != "pos"
+            and data_type != "pos_diff_start"
+        ):
+            plot_data = convert(
+                original_data, start_pos, data_type, start_xpos
+            ).reshape(nr_frames, 8, 3)
+        else:
+            plot_data = convert(original_data, start_pos, data_type).reshape(
+                nr_frames, 8, 3
+            )
 
         # Load original xyz position data for validating plot_data
         plot_data_true_pos = torch.tensor(
@@ -470,7 +488,7 @@ if __name__ == "__main__":
         "--data_dir",
         type=str,
         help="data directory",
-        default="data_t(10, 20)_r(0, 0)_tennis_pNone_gNone",
+        default="data_t(0, 0)_r(5, 10)_tennis_pNone_gNone",
     )
     parser.add_argument("--prediction", action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
@@ -542,16 +560,16 @@ if __name__ == "__main__":
     # -----------------------------------
     else:
         # Below the test for all datatypes
-        i = randint(0, nr_sims - 1)
-
-        print("simulation", i)
+        # i = randint(0, nr_sims - 1)
+        i = 0
+        # print("simulation", i)
         # Test all data types:
 
         data_types = [
             "pos",
-            "eucl_motion",
+            # "eucl_motion",
             # "eucl_motion_ori",
-            # "quat",
+            "quat",
             # "quat_ori",
             # "log_quat",
             # "log_quat_ori",