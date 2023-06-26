3f47dc531b2e9a6b5c697367dc6ada2dfc504f81
nreints <nienke.reints@gmail.com>
2023-06-26 12:16:48 +0200

    update code collect results

--- code/collect_results.py
+++ code/collect_results.py
@@ -2,6 +2,7 @@ import wandb
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 
 
 def get_runs():
@@ -15,88 +16,102 @@ def get_runs():
     return runs
 
 
-def get_filtered_runs(runs, filters):
+def get_grouped_filtered_runs(runs, filters, groups):
     filtered_runs = []
-    for run in runs[:50]:
+    grouped_runs = {}
+    for run in runs:
         config = run.config
 
         # If it satisfies all filters, save it
         if all([config.get(filter) == filters[filter] for filter in filters]):
             filtered_runs += [run]
-        # for filter in filters:
-        #     if config.get(filter) == filters[filter]:
-        # filtered_runs += [run]
-        # key = [config.get(filter) for filter in filters]
-        # print(key)
-    print(f"{len(filtered_runs)} runs satisfy the filters")
-    return filtered_runs
+            config = run.config
+
+            key = tuple([config.get(group) for group in groups])
+
+            if key in grouped_runs:
+                grouped_runs[key].append(run)
+            else:
+                grouped_runs[key] = [run]
+    print(f"{len(filtered_runs)} runs satisfy the filters {filters}.")
+    return filtered_runs, grouped_runs
 
 
-def group_runs(runs, groups):
-    grouped_runs = {}
-    for run in runs:
-        config = run.config
+# def group_runs(runs, groups):
+#     grouped_runs = {}
+#     for run in runs:
+#         config = run.config
 
-        # group_thing = config.get(groups[0])
-        # group2_thing = config.get(groups[1])
-        # print(type(group_thing))
-        # print(type(group2_thing))
-        # key_try = (group_thing, group2_thing)
-        # print(key_try, type(key_try))
+#         key = tuple([config.get(group) for group in groups])
 
-        key = tuple([config.get(group) for group in groups])
-        # print(config.get(groups[0]))
-        # print(keys)
-        # for key in key_gen:
-        #     print(type(key))
-        # print(type(key_gen))
-        # exit()
-
-        if key in grouped_runs:
-            grouped_runs[key].append(run)
-        else:
-            grouped_runs[key] = [run]
-    return grouped_runs
+#         if key in grouped_runs:
+#             grouped_runs[key].append(run)
+#         else:
+#             grouped_runs[key] = [run]
+#     return grouped_runs
 
 
 def average_runs(group_dict):
-    values_mean = {}
+    data = {
+        "reference": [],
+        "str_extra_input": [],
+        "identity_focus": [],
+        "data_type": [],
+        "data_dir_train": [],
+        "mean_min_train": [],
+        "mean_min_test": [],
+    }
     for key, runs in (group_dict).items():
+        data["reference"] += [key[0]]
+        data["str_extra_input"] += [key[1]]
+        data["identity_focus"] += [key[2]]
+        data["data_type"] += [key[3]]
+        print(f"Considering runs of group {key}")
         train_loss_values = np.zeros((len(runs), runs[0].config.get("epochs")))
         test_loss_values = np.zeros((len(runs), runs[0].config.get("epochs")))
-        values_mean["epoch"] = range(runs[0].config.get("epochs"))
         for i, run in enumerate(runs):
             history = run.history()
-            # train_loss_values.extend(history.get("Train loss", []))
-            # test_loss_values.extend(history.get("Train loss", []))
             train_loss_values[i] = history.get("Train loss")
             test_loss_values[i] = history.get(
                 "Test loss t(5,20)_r(5,20)_combi_pNone_gNone"
             )
+        data["data_dir_train"] = run.config["data_dir_train"]
 
-        mean_group_train = np.mean(train_loss_values, axis=0)
-        std_group_train = np.std(train_loss_values, axis=0)
-        max_mean_group_train = np.max(mean_group_train, axis=0)
-        min_mean_group_train = np.min(mean_group_train, axis=0)
-        values_mean[runs[0].config.get("data_type") + " mean train"] = mean_group_train
+        min_vals_train = np.min(train_loss_values, axis=1)
+        mean_min_train = np.mean(min_vals_train)
+        data["mean_min_train"] = mean_min_train
+        min_vals_test = np.min(test_loss_values, axis=1)
+        mean_min_test = np.mean(min_vals_test)
+        data["mean_min_test"] = mean_min_test
 
-        mean_group_test = np.mean(train_loss_values, axis=0)
-        std_group_test = np.std(train_loss_values, axis=0)
-        max_mean_group_test = np.max(mean_group_test, axis=0)
-        min_mean_group_test = np.min(mean_group_test, axis=0)
-        values_mean[runs[0].config.get("data_type") + " mean test"] = mean_group_test
-        # print(mean_group)
-        # print(std_group)
-        # print(max_mean_group)
-        # print(min_mean_group)
-        # print(values_mean)
-    df = pd.DataFrame(values_mean)
-    train_columns = df.columns[df.columns.str.contains(pat="train")]
-    print(train_columns)
-    test_columns = df.columns[df.columns.str.contains(pat="test")]
-    df.plot(x="epoch", y=train_columns, kind="line", logy=True)
-    df.plot(x="epoch", y=test_columns, kind="line", logy=True)
-    plt.show()
+    df = pd.DataFrame(data)
+    df.to_pickle("results.pickle")
+    return df
+
+    #     mean_group_train = np.mean(train_loss_values, axis=0)
+    #     std_group_train = np.std(train_loss_values, axis=0)
+    #     max_mean_group_train = np.max(mean_group_train, axis=0)
+    #     min_mean_group_train = np.min(mean_group_train, axis=0)
+    #     values_mean[runs[0].config.get("data_type") + " mean train"] = mean_group_train
+
+    #     mean_group_test = np.mean(test_loss_values, axis=0)
+    #     std_group_test = np.std(train_loss_values, axis=0)
+    #     max_mean_group_test = np.max(mean_group_test, axis=0)
+    #     min_mean_group_test = np.min(mean_group_test, axis=0)
+    #     values_mean[runs[0].config.get("data_type") + " mean test"] = mean_group_test
+    #     # print(mean_group)
+    #     # print(std_group)
+    #     # print(max_mean_group)
+    #     # print(min_mean_group)
+    #     # print(values_mean)
+    # df = pd.DataFrame(values_mean)
+    # train_columns = df.columns[df.columns.str.contains(pat="train")]
+    # print(train_columns)
+    # print(train_columns)
+    # test_columns = df.columns[df.columns.str.contains(pat="test")]
+    # df.plot(x="epoch", y=train_columns, kind="line", logy=True)
+    # df.plot(x="epoch", y=test_columns, kind="line", logy=True)
+    # plt.show()
 
     # if train_loss_values:
     #     # Convert list to a numpy array for calculations
@@ -114,20 +129,48 @@ def average_runs(group_dict):
     #     print(f"For group {key}, no train_loss values found.")
 
 
-if __name__ == "__main__":
-    runs = get_runs()
-    filtered_runs = get_filtered_runs(
-        runs,
-        {
-            "focus_identity": False,
-            "data_dir_train": "data_t(5,20)_r(5,20)_combi_pNone_gNone",
-            "reference": "fr-fr",
-        },
+def get_specific_values(data=None):
+    if not data:
+        data = pd.read_pickle("results.pickle")
+    # print(data.loc[(data["reference"] == "fr-fr")])
+    print(data)
+    my_dict = {"identity_focus": None, "data_type": "log_quat_1"}
+    # my_dict = {"reference": "fr-fr"}
+    # my_dict = {"identity_focus": None}
+    mask = np.logical_and.reduce(
+        [pd.isnull(data[k]) if v is None else data[k] == v for k, v in my_dict.items()]
     )
-    group_by = ["data_type"]
-    grouped_runs = group_runs(filtered_runs, group_by)
+    print(data.loc[mask])
+    # print(data.loc[(data['column_name'] >= A) & (data['column_name'] <= B)])
 
-    average_data = average_runs(grouped_runs)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n",
+        "--new_collect_results",
+        action="store_true",
+        help="Force the model to focus on identity",
+    )
+    args = parser.parse_args()
+    if args.new_collect_results:
+        runs = get_runs()
+        group_by = ["reference", "str_extra_input", "identity_focus", "data_type"]
+        filtered_runs, grouped_runs = get_grouped_filtered_runs(
+            runs,
+            {
+                # "reference": "fr-fr",
+                "str_extra_input": None,
+                "identity_focus": False,
+                "data_dir_train": "data_t(5,20)_r(5,20)_combi_pNone_gNone",
+            },
+            group_by,
+        )
+        average_data = average_runs(grouped_runs)
+        specific_df = get_specific_values(average_data)
+    else:
+        print("Using already collected runs !(These may be old)!")
+        specific_df = get_specific_values()
     # history = run.history()
     #     print(history)
     #     for col in history:
